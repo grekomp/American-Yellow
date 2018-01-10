663648d92eb5e8c565592a11131a23407ca84740
Grzegorz Palian <greekomp@gmail.com>
2018-01-10 16:57:41 +0100

    Added flower wind animation

---
+++ Assets/Scripts/FlowerWindMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerWindMovement : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public float magnitude = 10.0f;
+
+	public float timeRandomness = 0.1f;
+
+	float positionXMultiplier = 1.0f;
+	float positionYMultiplier = 2.0f;
+
+	float timeElapsed = 0;
+	float timeOffset;
+
+	void Start () {
+		timeOffset = Random.Range(0, timeRandomness);
+		timeOffset += transform.position.x * positionXMultiplier;
+		timeOffset += transform.position.y * positionYMultiplier;
+	}
+	
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Cos(timeElapsed * speed + timeOffset) * magnitude));
+
+
+	}
+}
